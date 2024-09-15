c7f9de4028ff379747269030f61725d42e68e61b
illidan <abdulahadkashem@gmail.com>
2024-09-16 00:10:33 +0600

    59th commit

---
+++ Medium/1371. Find the Longest Substring Containing Vowels in Even Counts.py
@@ -0,0 +1,22 @@
+def findTheLongestSubstring(s):
+    vowels = "aeiou"
+
+    res = 0
+    mask = 0
+    mask_to_idx = {0: -1}
+
+    for i, c in enumerate(s):
+        if c in vowels:
+            mask = mask ^ (1 + ord(c) - ord("a"))
+
+        if mask in mask_to_idx:
+            length = i - mask_to_idx[mask]
+            res = max(res, length)
+        else:
+            mask_to_idx[mask] = i
+
+    return res
+
+
+s = "eleetminicoworoep"
+print(findTheLongestSubstring(s))
