3b190d1185bc5872fcca7b7df8a12133887042a8
Ill1dan <abdulahadkashem@gmail.com>
2025-12-09 17:34:42 +0600

    Medium

---
+++ Medium/3583. Count Special Triplets.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def specialTriplets(self, nums):
+    mod = 10 ** 9 + 7
+
+    ans, n = 0, len(nums)
+    left_count = Counter()
+    right_count = Counter(nums)
+    for i in range(n):
+        right_count[nums[i]] -= 1
+        target = nums[i] * 2
+        ans += (left_count[target] * right_count[target]) % mod
+
+        left_count[nums[i]] += 1
+
+    return ans % mod
