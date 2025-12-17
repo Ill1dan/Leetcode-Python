7b7e0d848d366b17732e5163bc870d26200df2e7
Ill1dan <abdulahadkashem@gmail.com>
2025-12-17 22:04:46 +0600

    Medium

---
+++ Medium/3573. Best Time to Buy and Sell Stock V.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def maximumProfit(self, prices, k):
+        n = len(prices)
+        if n < 2 or k == 0:
+            return 0
+
+        dp_prev = [0] * n
+        dp_cur = [0] * n
+
+        for t in range(1, k + 1):
+            best_buy = -prices[0]  # Best buy profit
+            best_short = prices[0]     # Best short sell profit
+            dp_cur[0] = 0
+
+            for i in range(1, n):
+                a = dp_cur[i - 1]
+                b = best_buy + prices[i]
+                c = best_short - prices[i]
+
+                dp_cur[i] = max(a, b, c)
+
+                best_buy = max(best_buy, dp_prev[i - 1] - prices[i])
+                best_short = max(best_short, dp_prev[i - 1] + prices[i])
+
+            dp_prev, dp_cur = dp_cur, dp_prev
+
+        return dp_prev[-1]
+        
