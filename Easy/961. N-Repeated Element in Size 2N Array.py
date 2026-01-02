e3d6a61e5bbe773c0e49a9b269c4feeea57a4400
Ill1dan <abdulahadkashem@gmail.com>
2026-01-02 22:36:32 +0600

    Easy

---
+++ Easy/961. N-Repeated Element in Size 2N Array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def repeatedNTimes(self, nums):
+        seen = set()
+
+        for i in nums:
+            if i not in seen:
+                seen.add(i)
+            else:
+                return i
+        
